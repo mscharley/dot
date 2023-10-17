5efef0c41fa7af7f651c4ca6aaf761c9394be36c
Matthew Scharley <matt@scharley.me>
2023-10-17 19:37:00 +1100

    chore: improve coverage

---
+++ src/util/__tests__/injectionFromIdentifier.ts
@@ -0,0 +1,50 @@
+import { describe, expect, it } from '@jest/globals';
+import type { Injection } from '../../models/Injection.js';
+import { injectionFromIdentifier } from '../injectionFromIdentifier.js';
+import { Token } from '../../Token.js';
+import { unmanaged } from '../../decorators/unmanaged.js';
+import { withOptions } from '../../decorators/withOptions.js';
+
+const token = new Token<string>('test');
+
+describe('injectionFromIdentifier', () => {
+	it('generates for a token', () => {
+		expect(injectionFromIdentifier(token, 0)).toEqual({
+			type: 'constructorParameter',
+			id: token,
+			index: 0,
+			options: { multiple: false, optional: false },
+		} satisfies Injection<string>);
+	});
+
+	it('generates for a token with options', () => {
+		expect(injectionFromIdentifier(withOptions(token, { multiple: true }), 0)).toEqual({
+			type: 'constructorParameter',
+			id: token,
+			index: 0,
+			options: { multiple: true, optional: false },
+		} satisfies Injection<string>);
+	});
+
+	it('generates for a constructor', () => {
+		class Test {}
+
+		expect(injectionFromIdentifier(Test, 0)).toEqual({
+			type: 'constructorParameter',
+			id: Test,
+			index: 0,
+			options: { multiple: false, optional: false },
+		} satisfies Injection<Test>);
+	});
+
+	it('generates for a direct injection', () => {
+		const id = unmanaged('foobar');
+		expect(injectionFromIdentifier(id, 0)).toEqual({
+			type: 'unmanagedConstructorParameter',
+			id: id.token,
+			index: 0,
+			options: { multiple: false, optional: false },
+			value: id,
+		} satisfies Injection<string>);
+	});
+});
