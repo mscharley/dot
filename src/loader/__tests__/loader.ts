36323d1c507a9f759b29e9dbd456a51dc03dc3eb
Matthew Scharley <matt@scharley.me>
2024-09-27 10:20:30 +1000

    fix: add loader for adding metadata

---
+++ src/loader/__tests__/loader.ts
@@ -0,0 +1,100 @@
+/* eslint-disable jest/expect-expect */
+import { describe, expect, it, jest } from '@jest/globals';
+import { load } from '../index.js';
+import type { LoadResult } from '../index.js';
+
+describe('node-loader', () => {
+	const next = jest.fn<(url: string, context: unknown) => Promise<LoadResult>>();
+
+	const expectLoadContents = (contents: string): ReturnType<typeof expect<Promise<string>>> => {
+		next.mockImplementationOnce(async () => Promise.resolve({
+			source: Buffer.from(contents, 'utf8'),
+		}));
+
+		// eslint-disable-next-line jest/valid-expect
+		return expect(load('test.js', { format: 'module' }, next).then((v) => v.source.toString('utf8')));
+	};
+
+	it('can inject for a constant export', async () => {
+		await expectLoadContents('export const foo = {};').resolves.toMatchInlineSnapshot(`
+"export const foo = {};
+if (foo != null && ['function', 'object'].includes(typeof foo)) foo[Symbol.for('__dot_import_stats')] = {"url":"test.js","name":"foo"};"
+`);
+	});
+
+	it('can inject for a let export', async () => {
+		await expectLoadContents('export let foo = {};').resolves.toMatchInlineSnapshot(`
+"export let foo = {};
+if (foo != null && ['function', 'object'].includes(typeof foo)) foo[Symbol.for('__dot_import_stats')] = {"url":"test.js","name":"foo"};"
+`);
+	});
+
+	it('can inject for an exported class', async () => {
+		await expectLoadContents('export class Test {};').resolves.toMatchInlineSnapshot(`
+"export class Test {};
+if (Test != null && ['function', 'object'].includes(typeof Test)) Test[Symbol.for('__dot_import_stats')] = {"url":"test.js","name":"Test"};"
+`);
+	});
+
+	it('can inject for an exported function', async () => {
+		await expectLoadContents('export function test() {};').resolves.toMatchInlineSnapshot(`
+"export function test() {};
+if (test != null && ['function', 'object'].includes(typeof test)) test[Symbol.for('__dot_import_stats')] = {"url":"test.js","name":"test"};"
+`);
+	});
+
+	it('can inject an exported object destructure', async () => {
+		await expectLoadContents('export const { test } = { test: {} };').resolves.toMatchInlineSnapshot(`
+"export const { test } = { test: {} };
+if (test != null && ['function', 'object'].includes(typeof test)) test[Symbol.for('__dot_import_stats')] = {"url":"test.js","name":"test"};"
+`);
+	});
+
+	it('can inject an exported array destructure', async () => {
+		await expectLoadContents('export const [test] = [{}];').resolves.toMatchInlineSnapshot(`
+"export const [test] = [{}];
+if (test != null && ['function', 'object'].includes(typeof test)) test[Symbol.for('__dot_import_stats')] = {"url":"test.js","name":"test"};"
+`);
+	});
+
+	it('can\'t inject for a default export of an expression', async () => {
+		await expectLoadContents('export default ``;').resolves.toMatchInlineSnapshot(`
+"export default \`\`;
+"
+`);
+		await expectLoadContents('export default "";').resolves.toMatchInlineSnapshot(`
+"export default "";
+"
+`);
+		await expectLoadContents('export default {};').resolves.toMatchInlineSnapshot(`
+"export default {};
+"
+`);
+		await expectLoadContents('export default [];').resolves.toMatchInlineSnapshot(`
+"export default [];
+"
+`);
+		await expectLoadContents('export default function() {};').resolves.toMatchInlineSnapshot(`
+"export default function() {};
+"
+`);
+		await expectLoadContents('export default (function foo() {});').resolves.toMatchInlineSnapshot(`
+"export default (function foo() {});
+"
+`);
+	});
+
+	it('can inject for a default export of a named function', async () => {
+		await expectLoadContents('export default function foo() {};').resolves.toMatchInlineSnapshot(`
+"export default function foo() {};
+if (foo != null && ['function', 'object'].includes(typeof foo)) foo[Symbol.for('__dot_import_stats')] = {"url":"test.js","name":"foo"};"
+`);
+	});
+
+	it('can inject for a default export of an identifier', async () => {
+		await expectLoadContents('const foo = {}; export default foo;').resolves.toMatchInlineSnapshot(`
+"const foo = {}; export default foo;
+if (foo != null && ['function', 'object'].includes(typeof foo)) foo[Symbol.for('__dot_import_stats')] = {"url":"test.js","name":"foo"};"
+`);
+	});
+});
---
+++ src/loader/index.ts
@@ -0,0 +1,116 @@
+/* eslint-disable no-console */
+
+import type { AssignmentProperty, ModuleDeclaration, Statement, VariableDeclarator } from 'acorn';
+import { Parser } from 'acorn';
+
+// TODO: TypeScript itself doesn't seem to provide typing for these things, but ts-node has some types that may be worth investigating.
+// https://github.com/TypeStrong/ts-node/blob/main/src/esm.ts
+
+interface ExportEntry {
+	url: string;
+	name: string;
+}
+
+// export async function resolve(specifier: string, context: any, next: (specifier: string, context: any) => Promise<any>): Promise<any> {
+// 	return next(specifier, context);
+// }
+
+const generateMetadataAssignment = (meta: ExportEntry): string =>
+	`if (${meta.name} != null && ['function', 'object'].includes(typeof ${meta.name})) ${meta.name}[Symbol.for('__dot_import_stats')] = ${JSON.stringify(meta)};`;
+
+const variableDeclaratorEntries = (url: string) => (d: VariableDeclarator): ExportEntry[] => {
+	switch (d.id.type) {
+		case 'Identifier':
+			return [{ url, name: d.id.name }];
+
+		case 'ObjectPattern':
+			return d.id.properties
+				.filter((prop): prop is AssignmentProperty => prop.type === 'Property')
+				.flatMap((prop) => prop.value.type === 'Identifier' ? [{ url, name: prop.value.name }] : []);
+
+		case 'ArrayPattern':
+			return d.id.elements
+				.flatMap((prop) => (prop != null && prop.type === 'Identifier') ? [{ url, name: prop.name }] : []);
+
+		default:
+			console.warn(url, 'Unknown variable declaration in named export:', JSON.stringify(d, undefined, 2));
+			return [];
+	}
+};
+
+const exportEntries = (url: string) => (node: Statement | ModuleDeclaration): ExportEntry[] => {
+	switch (node.type) {
+		case 'ExportNamedDeclaration':
+			switch (node.declaration?.type) {
+				case 'FunctionDeclaration':
+					return [{ url, name: node.declaration.id.name }];
+
+				case 'VariableDeclaration':
+					return node.declaration.declarations.flatMap(variableDeclaratorEntries(url));
+
+				case 'ClassDeclaration':
+					return [{ url, name: node.declaration.id.name }];
+
+				default:
+					return [];
+			}
+
+		case 'ExportDefaultDeclaration':
+			switch (node.declaration.type) {
+				case 'Identifier':
+					return [{ url, name: node.declaration.name }];
+
+				case 'FunctionDeclaration':
+					if (node.declaration.id == null) {
+						return [];
+					} else {
+						return [{ url, name: node.declaration.id.name }];
+					}
+
+				case 'Literal':
+				case 'TemplateLiteral':
+				case 'ObjectExpression':
+				case 'ArrayExpression':
+				case 'FunctionExpression':
+					return [];
+
+				default:
+					console.warn(url, 'Unknown default declaration type:', JSON.stringify(node, undefined, 2));
+					return [];
+			}
+		default:
+			return [];
+	}
+};
+
+export interface LoadContext {
+	format: string;
+}
+
+export interface LoadResult {
+	source: Buffer;
+}
+
+export async function load(url: string, context: LoadContext, next: (url: string, context: LoadContext) => Promise<LoadResult>): Promise<LoadResult> {
+	const result = await next(url, context);
+	if (context.format !== 'module') {
+		return result;
+	}
+
+	const parsed = Parser.parse(result.source.toString('utf8'), {
+		sourceType: 'module',
+		ecmaVersion: 'latest',
+	});
+
+	const exports = parsed.body.flatMap(exportEntries(url)).map(generateMetadataAssignment);
+
+	const source = Buffer.concat([
+		result.source,
+		Buffer.from('\n'),
+		Buffer.from(exports.join('\n'), 'utf8'),
+	]);
+	return {
+		...result,
+		source,
+	};
+}
