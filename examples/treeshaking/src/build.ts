ced37350d5a4e721b23ebae7b9b40b9f5d626794
Matthew Scharley <matt@scharley.me>
2024-10-03 18:38:29 +1000

    fix: migrate ncc => rollup

--- examples/treeshaking/src/build.ts
+++ examples/treeshaking/src/build.ts
@@ -1,7 +1,10 @@
+import { dirname, join, relative } from 'node:path';
+import { fileURLToPath, pathToFileURL } from 'node:url';
 import type { interfaces } from '@mscharley/dot';
 import { mkdirp } from 'fs-extra/esm';
-import { pathToFileURL } from 'node:url';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { register } from 'node:module';
+import { rollup } from 'rollup';
 import { writeFile } from 'node:fs/promises';
 
 await mkdirp('./dist/opt');
@@ -15,9 +18,11 @@ const c = await generateContainer();
 const deps = main.dependencies.map((d): interfaces.ContainerModuleMeta => (d as any)[Symbol.for('__dot_import_stats')]);
 const modules = c.getRequiredContainerModules(main.dependencies);
 
-await writeFile('./dist/opt/index.js', `import { createContainer } from "@mscharley/dot";
-${modules.map(({ name, url }) => `import { ${name} } from "${url}";`).join('\n')}
-${deps.map(({ name, url }) => `import { ${name} } from "${url}";`).join('\n')}
+const optimisedOutputFile = './dist/opt/index.js';
+await writeFile(optimisedOutputFile, `import { createContainer } from "@mscharley/dot";
+
+${modules.map(({ name, url }) => `import { ${name} } from "${relative(dirname(optimisedOutputFile), fileURLToPath(url))}";`).join('\n')}
+${deps.map(({ name, url }) => `import { ${name} } from "${relative(dirname(optimisedOutputFile), fileURLToPath(url))}";`).join('\n')}
 
 console.log("Code loaded.");
 
@@ -25,8 +30,39 @@ export const main = async () => {
     const c = createContainer();
 ${modules.map(({ name }) => `    await c.load(${name});`).join('\n')}
 
-    const args = await Promise.all([${deps.map(({ name }, i) => `c.get(${name})`).join('\n')}]);
+    const args = await Promise.all([${deps.map(({ name }) => `c.get(${name})`).join('\n')}]);
 
     (${main.handler.toString()})(...args);
 };
 `);
+
+async function rollupOutput(input: string, outputDir: string): Promise<void> {
+	const bundle = await rollup({
+		treeshake: true,
+		input,
+		plugins: [nodeResolve({
+			exportConditions: ['node'],
+		})],
+	});
+
+	await mkdirp(outputDir);
+	const { output } = await bundle.generate({
+		dir: outputDir,
+		format: 'esm',
+	});
+
+	for (const chunk of output) {
+		if (chunk.type === 'asset') {
+			// eslint-disable-next-line no-await-in-loop
+			await writeFile(join(outputDir, chunk.fileName), chunk.source);
+		} else {
+			// eslint-disable-next-line no-await-in-loop
+			await writeFile(join(outputDir, chunk.fileName), chunk.code);
+		}
+	}
+
+	await bundle.close();
+}
+
+await rollupOutput('dist/index.js', 'dist/bundled');
+await rollupOutput('dist/opt/index.js', 'dist/bundled.opt');
