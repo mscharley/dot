ea8b66ce1abd62f5a293ebebb423179f21221eaa
Matthew Scharley <matt@scharley.me>
2024-10-07 16:00:32 +1100

    chore: migrate to MPL 2.0 license

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import { configs, withStyles } from '@mscharley/eslint-config';
+import notice from 'eslint-plugin-notice';
 
 export default [
 	...configs.recommended,
@@ -20,6 +21,25 @@ export default [
 		],
 		rules: {
 			'@typescript-eslint/no-type-alias': 'off',
+		},
+	},
+	{
+		plugins: { notice },
+		files: ['src/**/*.{ts,js,tsx,jsx}'],
+		rules: {
+			'notice/notice': [
+				'error',
+				{
+					template:
+						'/**\n'
+						+ ' * This Source Code Form is subject to the terms of the Mozilla Public\n'
+						+ ' * License, v. 2.0. If a copy of the MPL was not distributed with this\n'
+						+ ' * file, You can obtain one at http://mozilla.org/MPL/2.0/.\n'
+						+ ' */\n\n',
+					// The package entrypoint needs some extra grace since the first comment must be the @packageDocumentation.
+					chars: 5000,
+				},
+			],
 		},
 	},
 ];
